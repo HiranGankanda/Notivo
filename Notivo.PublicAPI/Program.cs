3a82954aa9da0505350b1343bd0c89f2b8d07b90
Hiran Gankanda <hirangankanda@gmail.com>
2025-01-13 13:39:29 +0530

    Update 01

--- Notivo.PublicAPI/Program.cs
+++ Notivo.PublicAPI/Program.cs
@@ -1,11 +1,13 @@
+using MediatR;
+using Notivo.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(typeof(Program).Assembly); // This links MediatR to the Application project
 
 var app = builder.Build();
 
